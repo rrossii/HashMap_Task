0cb4326cf69b2d87ca00f70b7fc3f66480f6d3ab
Codefinity-Java <dl@codefinity.com>
2023-11-29 16:26:41 +0200

    Added task

--- src/main/java/codefinity/Task/OnlineStoreDatabase.java
+++ src/main/java/codefinity/Task/OnlineStoreDatabase.java
@@ -1,2 +1,24 @@
-package codefinity.Task;public class OnlineStoreDatabase {
+package codefinity.Task;
+
+import codefinity.model.Customer;
+import codefinity.model.Product;
+
+public interface OnlineStoreDatabase {
+    void addProduct(Product product);
+
+    void updateProduct(int productId, double newPrice, int newQuantity);
+
+    void removeProduct(int productId);
+
+    void addCustomer(Customer customer);
+
+    void updateCustomer(int customerId, String newAddress);
+
+    void removeCustomer(int customerId);
+
+    void placeOrder(int customerId, int productId, int quantity);
+
+    void displayAllProducts();
+
+    void displayAllCustomers();
 }
--- src/main/java/codefinity/Task/OnlineStoreDatabaseImpl.java
+++ src/main/java/codefinity/Task/OnlineStoreDatabaseImpl.java
@@ -1,2 +1,64 @@
-package codefinity.Task;public class OnlineStoreDatabaseImpl {
+package codefinity.Task;
+
+import codefinity.model.Customer;
+import codefinity.model.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OnlineStoreDatabaseImpl implements OnlineStoreDatabase {
+
+    private Map<Integer, Product> products;
+    private Map<Integer, Customer> customers;
+
+    public OnlineStoreDatabaseImpl() {
+        this.products = new HashMap<>();
+        this.customers = new HashMap<>();
+    }
+
+
+    @Override
+    public void addProduct(Product product) {
+
+    }
+
+    @Override
+    public void updateProduct(int productId, double newPrice, int newQuantity) {
+
+    }
+
+    @Override
+    public void removeProduct(int productId) {
+
+    }
+
+    @Override
+    public void addCustomer(Customer customer) {
+
+    }
+
+    @Override
+    public void updateCustomer(int customerId, String newAddress) {
+
+    }
+
+    @Override
+    public void removeCustomer(int customerId) {
+
+    }
+
+    @Override
+    public void placeOrder(int customerId, int productId, int quantity) {
+
+    }
+
+    @Override
+    public void displayAllProducts() {
+
+    }
+
+    @Override
+    public void displayAllCustomers() {
+
+    }
 }
